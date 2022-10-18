d490b1bb202d6bb8e835fd1f33754f132f819561
BabychOleksii <babych.oleksii@gmail.com>
2022-10-18 23:34:44 +0300

    Functions

---
+++ 01_Python_basics/for_loop_example.py
@@ -0,0 +1,20 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+
+for number in range(12, 16):
+    print('number is ' + str(number))
+
+
+for odd_number in range(2, 10, 2):
+    print('odd number is ' + str(odd_number))
+
+
+for rev_number in range(10, -10, -3):
+    print('rev number is ' + str(rev_number))
---
+++ 02_Functions/pyperclip_copy_paste.py
@@ -0,0 +1,5 @@
+#run these commands one by one in the Python IDLE Shell
+import pyperclip
+
+pyperclip.copy('Hello world!')
+pyperclip.paste()
